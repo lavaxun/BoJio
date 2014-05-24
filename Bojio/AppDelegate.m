cd8cce5cc70f3c40586190d64115fba33d939bea
Biranchi <biranchi125@gmail.com>
2014-05-24 14:35:33 +0800

    first commit

--- Bojio/AppDelegate.m
+++ Bojio/AppDelegate.m
@@ -13,6 +13,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+  //Sample Test
+  
     return YES;
 }
 							
